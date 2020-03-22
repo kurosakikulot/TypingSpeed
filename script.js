f60123281805f44197bcc59069d1427efa42b4d5
kurosakikulot <50890128+kurosakikulot@users.noreply.github.com>
2020-03-23 06:58:36 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
+const quoteDisplayElement = document.getElementById("quoteDisplay");
+const quoteInputElement = document.getElementById("quoteInput");
+const timerElement = document.getElementById("timer");
+const speedElement = document.getElementById("speed");
+const playButton = document.querySelector("button");
+
+quoteInputElement.addEventListener("input", () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
+  const arrayValue = quoteInputElement.value.split("");
+
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.add("incorrect");
+      correct = false;
+    }
+  });
+
+  if (correct) {
+    const spans = document.querySelectorAll(".correct");
+    const word = spans.length / 5;
+    const speed = Math.round((word / timerElement.innerHTML) * 60);
+    timerElement.style.display = "none";
+    speedElement.innerHTML = `Your speed is ${speed} words per minute`;
+    playButton.innerHTML = "Play Again";
+  }
+});
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content);
+}
+
+async function renderNewQuote() {
+  timerElement.style.display = "block";
+  speedElement.innerHTML = "";
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerText = "";
+  quote.split("").forEach(character => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
+  startTimer();
+}
+
+let startTime;
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
+}
